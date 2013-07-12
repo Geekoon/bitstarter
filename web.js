b19db47fe55fa44e28b03c4c5da74cea4a7c881e
Anton Orlovskikh <orlovskikh@gmail.com>
2013-07-12 07:45:37 +0000

    Read from file

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
+
+var buf = new Buffer(256);
+fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString('utf8', 0, len));
 });
 
 var port = process.env.PORT || 5000;
